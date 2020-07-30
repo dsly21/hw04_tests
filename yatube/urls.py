c3961d2eded09ac421ff7fad42926f67c2cfab09
Nikolay_Zelyukov <nikolaizelyukov@yandex.ru>
2020-07-30 17:47:59 +1000

    change path flat pages

--- yatube/urls.py
+++ yatube/urls.py
@@ -4,20 +4,17 @@ from django.urls import include, path
 from django.contrib.flatpages import views
 
 urlpatterns = [
+    path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+    path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+    path('about-author/', views.flatpage, {'url': '/about-author/'},
+         name='author'),
+
+    path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
     path("", include("posts.urls")),
     path("auth/", include("users.urls")),
     path('about/', include('django.contrib.flatpages.urls')),
     path("auth/", include("django.contrib.auth.urls")),
     path("admin/admin/", admin.site.urls),
-]
-urlpatterns += [
-        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
-        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
-        path('about-author/', views.flatpage, {'url': '/about-author/'},
-             name='author'),
-
-        path('about-spec/', views.flatpage, {'url': '/about-spec/'},
-             name='spec'),
 ]
 
 
